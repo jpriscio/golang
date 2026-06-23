f09d2469cb76e2a71ba6a5f5cd88b89373551b0c
joao priscio <priscioj@gmail.com>
2026-06-22 22:40:40 -0300

    Chamada http

---
+++ Pacotes/Chamada_http/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+
+	req, err := http.Get("https://google.com.br")
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := io.ReadAll(req.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	println(string(res))
+	req.Body.Close()
+}
