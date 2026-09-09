e5d62d09ea09dd67858d98baf85098afc4b8e020
joao priscio <priscioj@gmail.com>
2026-09-09 11:31:50 -0300

    copia simples de slice

---
+++ Fundamentos/Array-vs-Slice/go.mod
@@ -0,0 +1,3 @@
+module github.com/jpriscio/golang/fundamentos/array-vs-slice
+
+go 1.25.0
---
+++ Fundamentos/Array-vs-Slice/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := make([]int, 10)
+
+	copy(slice2, slice1)
+	fmt.Println(slice2)
+
+}
